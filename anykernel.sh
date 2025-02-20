4b96c116563a440c06f12fd38e561d760d64cee0
James McConnell <bins4us@hotmail.com>
2025-02-20 01:03:50 -0500

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Wild Plus Kernel by The Wild James / Morgan Weedman
+kernel.string=Wild Plus Kernel by The Wild James aka Morgan Weedman
 do.devicecheck=0
 do.modules=0
 do.systemless=0
