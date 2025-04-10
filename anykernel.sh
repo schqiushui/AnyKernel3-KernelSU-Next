ae55532582e76e81c7d39f369d428fa501aa5647
schqiushui <orochi9999@gmail.com>
2025-04-10 21:58:30 +0800

    Update anykernel.sh
    Signed-off-by: schqiushui <orochi9999@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Wild Plus Kernel by TheWildJames or Morgan Weedman
+kernel.string=Oplus Kernel by Lotus
 do.devicecheck=0
 do.modules=0
 do.systemless=0
