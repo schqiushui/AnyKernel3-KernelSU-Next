cb2d7ecea42344b72d3b2e6acab3c7c65493ee30
schqiushui <orochi9999@gmail.com>
2025-06-06 21:40:07 +0800

    anykernel: Cleanup and update for gki kernels
    Signed-off-by: schqiushui <orochi9999@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 ### AnyKernel3 Ramdisk Mod Script
 ## osm0sis @ xda-developers
 
@@ -7,7 +9,7 @@ properties() { '
 kernel.string=Oplus Kernel by Lotus
 do.devicecheck=0
 do.modules=0
-do.systemless=0
+do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=
@@ -22,27 +24,16 @@ supported.vendorpatchlevels=
 
 
 ### AnyKernel install
-# boot shell variables
-BLOCK=boot;
-IS_SLOT_DEVICE=auto;
-RAMDISK_COMPRESSION=auto;
-PATCH_VBMETA_FLAG=0;
-NO_MAGISK_CHECK=1;
-NO_VBMETA_PARTITION_PATCH=1;
+## boot shell variables
+block=boot
+is_slot_device=1
+ramdisk_compression=auto
+patch_vbmeta_flag=0
+no_magisk_check=1
+no_vbmeta_partition_patch=1
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh
-
-kernel_version=$(cat /proc/version | awk -F '-' '{print $1}' | awk '{print $3}')
-case $kernel_version in
-    5.1*) ksu_supported=true ;;
-    6.1*) ksu_supported=true ;;
-    6.6*) ksu_supported=true ;;
-    *) ksu_supported=false ;;
-esac
-
-ui_print " " "  [✓] ksu_supported: $ksu_supported"
-$ksu_supported || abort "  [✗] Non-GKI device, abort."
 
 # boot install
 if [ -L "/dev/block/bootdevice/by-name/init_boot_a" -o -L "/dev/block/by-name/init_boot_a" ]; then
